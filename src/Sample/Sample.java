d8b2435dac6507602f2ecbed94e06778cc2dfd81
prithiv24 <prithivks24@gmail.com>
2022-07-06 22:05:56 +0530

    first commit

---
+++ src/Sample/Sample.java
@@ -0,0 +1,14 @@
+package Sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		String wife = "Raskshitha";
+		
+		StringBuffer name = new StringBuffer("Prithiv");
+		StringBuffer append = name.append(wife);
+		System.out.println(append);
+	}
+	
+
+}
+
